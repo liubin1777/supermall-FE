f71737bffc0468fdb7d96b491c9e4f5a410c7f04
robin <663907402@qq.com>
2022-05-24 09:04:13 +0800

    feat：添加图片

--- modules/business/home/components/KingPie/mock.js
+++ modules/business/home/components/KingPie/mock.js
@@ -1,11 +1,62 @@
-const data = [];
-for (let i = 0; i < 20; i++) {
-  data.push({
-    name: '超市' + i,
-    img: 'http://127.0.0.1:8080/assets/tabbar/icon-tabbar-home-n.png',
-    url: '/',
-  });
-}
+// const data = [];
+// for (let i = 0; i < 20; i++) {
+//   data.push({
+//     name: '超市',
+//     img: 'http://127.0.0.1:8080/assets/king/icon-king-supermarket.png',
+//     url: '/',
+//   });
+// }
 export default {
-  data,
+  data: [
+    {
+      name: '超市',
+      img: 'http://127.0.0.1:8080/assets/king/icon-king-supermarket.png',
+      url: '/',
+    },
+    {
+      name: '家电',
+      img: 'http://127.0.0.1:8080/assets/king/icon-king-electric-appliance.png',
+      url: '/',
+    },
+    {
+      name: '服饰',
+      img: 'http://127.0.0.1:8080/assets/king/icon-king-dress.png',
+      url: '/',
+    },
+    {
+      name: '水果',
+      img: 'http://127.0.0.1:8080/assets/king/icon-king-fruit.png',
+      url: '/',
+    },
+    {
+      name: '到家',
+      img: 'http://127.0.0.1:8080/assets/king/icon-king-arrive-home.png',
+      url: '/',
+    },
+    {
+      name: '生活缴费',
+      img: 'http://127.0.0.1:8080/assets/king/icon-king-fee.png',
+      url: '/',
+    },
+    {
+      name: '领金豆',
+      img: 'http://127.0.0.1:8080/assets/king/icon-king-golden-bean.png',
+      url: '/',
+    },
+    {
+      name: '优惠券',
+      img: 'http://127.0.0.1:8080/assets/king/icon-king-coupon.png',
+      url: '/',
+    },
+    {
+      name: '金贴',
+      img: 'http://127.0.0.1:8080/assets/king/icon-king-benefit.png',
+      url: '/',
+    },
+    {
+      name: 'PLUS会员',
+      img: 'http://127.0.0.1:8080/assets/king/icon-king-vip-plus.png',
+      url: '/',
+    }
+  ],
 };
