d23f6d45b14abd8eb82875fb0b93b6733aa2e7f6
robin <663907402@qq.com>
2022-05-29 21:17:48 +0800

    feat: 开发组件

---
+++ modules/business/home/components/Tabs/mock.js
@@ -0,0 +1,49 @@
+export default {
+  data: {
+    list: [
+      {
+        name: '推荐',
+      },
+      {
+        name: '食品',
+      },
+      {
+        name: '生鲜',
+      },
+      {
+        name: '手机',
+      },
+      {
+        name: '超市',
+      },
+      {
+        name: '家电',
+      },
+      {
+        name: '服饰',
+      },
+      {
+        name: '水果',
+      },
+      {
+        name: '到家',
+      },
+      {
+        name: '生活缴费',
+      },
+      {
+        name: '领金豆',
+      },
+      {
+        name: '优惠券',
+      },
+      {
+        name: '金贴',
+      },
+      {
+        name: 'PLUS会员',
+      },
+    ],
+    img: 'http://127.0.0.1:8080/assets/tabs/more.png'
+  },
+};
