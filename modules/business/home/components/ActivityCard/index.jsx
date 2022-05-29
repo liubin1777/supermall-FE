7f392f40462c38a8560da16031d0f0b19ebfcf83
robin <663907402@qq.com>
2022-05-29 18:17:10 +0800

    feat: 组件开发

---
+++ modules/business/home/components/ActivityCard/index.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import styles from './index.module.css';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+/**
+ * 活动组件
+ *
+ * @param {array} data: 商品数据
+ * @return {Object} ReactElement
+ */
+export default React.memo(({ data }) => {
+  if (!data) {
+    return null;
+  }
+
+  // 点击回调
+  function onClick() {
+    console.log('[SuperMall] ActivityCard|onClick');
+  }
+
+  console.log('[SuperMall] ActivityCard|render');
+  return (
+    <div className={styles.root}>
+      {/* 标题 */}
+      <div className={styles['title-container']}>
+        <div className={styles['title-name']}>惊喜直播</div>
+        <div className={styles['title-tag']}>新品先知道</div>
+      </div>
+      {/* 商品列表 */}
+      <div className={styles.goods}>
+        {new Array(2).fill(1).map((item) => {
+          return (
+            <div className={styles['item-layout']}>
+              <div className={styles.item}>
+                <img
+                  className={styles['goods-img']}
+                  src="http://127.0.0.1:8080/assets/activitycard/2.png"
+                  alt=""
+                />
+                <div className={styles['price-container']}>
+                  <div className={styles.price}>￥150.0</div>
+                  <div className={styles['original-price']}>￥180.9</div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+});
---
+++ modules/business/home/components/GoodsCard/index.jsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import styles from './index.module.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay } from 'swiper';
+
+/**
+ * 商品组件
+ *
+ * @param {array} data: 商品数据
+ * @return {Object} ReactElement
+ */
+export default React.memo(({ data }) => {
+  if (!data) {
+    return null;
+  }
+
+  // 点击回调
+  function onClick() {
+    console.log('[SuperMall] GoodsCard|onClick');
+  }
+
+  console.log('[SuperMall] GoodsCard|render');
+  return (
+    <div className={styles.root}>
+      {/* 商品图片 */}
+      <div className={styles['cover-container']}>
+        <img className={styles.img} src={data.img} />
+      </div>
+      {/* 商品信息 */}
+      <div className={styles.info}>
+        {/* 标题信息 */}
+        <div className={styles['title-container']}>
+          {/* 标签 */}
+          <div className={styles['label-container']}>
+            <div className={styles['label-layout']}>
+              <img
+                className={styles.label}
+                src="http://127.0.0.1:8080/assets/goodscard/label-chaoshi.png"
+              />
+            </div>
+          </div>
+          {/* 标题 */}
+          {data.title}
+        </div>
+
+        {/* 价格信息 */}
+        <div className={styles['price-container']}>
+          <div className={styles.price}>
+            ￥<span className={styles['price-int']}>388</span>
+            .00
+          </div>
+        </div>
+
+      </div>
+      {/* 看相似 */}
+      <div className={styles['show-similar']}>看相似</div>
+    </div>
+  );
+});
